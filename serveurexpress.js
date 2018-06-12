3c1cbfdd0e556bb56ab74a434e6d5f65aa46d296
Hamza Rguig <rguighamza0@gmail.com>
2018-06-12 15:41:52 +0200

    update

--- serveurexpress.js
+++ serveurexpress.js
@@ -24,15 +24,19 @@ connection.connect(function(err) {
  
  app.get('/',function(req,res){
   connection.query('SELECT * FROM mesures WHERE id = 1', function (error, results, fields) {
- res.send(results[0]);
+    releve = results[0];
+    console.log(releve);
+  res.send(
+    {
+      "success": true,
+    }
+  );
+
   })
 
 
+
  })
-
-
-   
-
  
 // respond with "hello world" when a GET request is made to the homepage
 app.post('/', function (req, res) {
@@ -40,6 +44,7 @@ app.post('/', function (req, res) {
 	console.log('New request')
 	console.log('start body')
 	console.log(req.body)
+
 	//console.log('end body');
 	//console.log('----------')
 	// Sauvgarde les donnees dans la base de donnee
@@ -48,17 +53,18 @@ app.post('/', function (req, res) {
   var temperature = req.body.temperature;
   var humidite = req.body.humidite;
   var temperature_liquide = req.body.temperature_liquide;
+  var Ph = req.body.Ph;
 
   connection.query('SELECT * FROM mesures WHERE id = 1', function (error, results, fields) {
   if (error) throw error;
     
     //Si il n'y a rien dans la base alors on ajoute sinon on met a jour
     if(results.length == 0){
-      connection.query('INSERT INTO mesures (temperature, humidite, temperature_liquide) VALUES(' + temperature + ', ' + humidite + ', ' + temperature_liquide + ')', function(error, results, fields){
+      connection.query('INSERT INTO mesures (temperature, humidite, temperature_liquide,Ph) VALUES(' + temperature + ', ' + humidite + ', ' + temperature_liquide + ','+Ph+')', function(error, results, fields){
         if (error) throw error;  
       });
     }else{
-      connection.query('UPDATE mesures SET temperature = ' + temperature + ', humidite = ' + humidite + ', temperature_liquide = ' + temperature_liquide + ' WHERE id = 1', function(error, results, fields){
+      connection.query('UPDATE mesures SET temperature = ' + temperature + ', humidite = ' + humidite + ', temperature_liquide = ' + temperature_liquide + ',ph = '+Ph+' WHERE id = 1', function(error, results, fields){
         if (error) throw error;  
       });
     }
