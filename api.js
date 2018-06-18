248052434f6e5d48b89b485fe535021318408920
alemoult <alexandre.lemoult3@etu.univ-lorraine.fr>
2018-06-18 14:02:22 +0200

    api

--- api.js
+++ api.js
@@ -1,32 +1,33 @@
 class Api{
 	
-	let user = {
-		"email": "clyx",
-		"password": "test"
-	};
-
-	let pousseslegumes = [
-		{
-			"id": 1,
-			"name": "Pousse Légume Salon",
-			"type": "Particulier"
-		},
-		{
-			"id": 2,
-			"name": "Pousse Légume Restaurant",
-			"type": "Professionnel"
-		},
-		{
-			"id": 3,
-			"name": "Pousse Légume Bite",
-			"type": "Professionnel"
-		}
-	];
-
-	let app;
+	
 	
 	constructor(app){
 		this.app = app;
+		
+		this.user = {
+			"email": "clyx",
+			"password": "test"
+		};
+
+		this.pousseslegumes = [
+			{
+				"id": 1,
+				"name": "Pousse Légume Salon",
+				"type": "Particulier"
+			},
+			{
+				"id": 2,
+				"name": "Pousse Légume Restaurant",
+				"type": "Professionnel"
+			},
+			{
+				"id": 3,
+				"name": "Pousse Légume Bite",
+				"type": "Professionnel"
+			}
+		];
+		
 	}
 
 	this.app.post('/login', function (req, res) {
@@ -34,7 +35,7 @@ class Api{
 	  
 	  console.log(req.body);
 	  
-	  if(req.body.email === user.email && req.body.password === user.password){
+	  if(req.body.email === this.user.email && req.body.password === this.user.password){
 		setTimeout(function(){
 			res.send({
 			  "success": true,
@@ -55,7 +56,7 @@ class Api{
 	this.app.get('/getpl', function(req, res){
 		res.send({
 		  "success": true,
-		  "pousse_legumes": pousseslegumes
+		  "pousse_legumes": this.pousseslegumes
 		});
 	});
 
